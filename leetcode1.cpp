a475756c8b97ceedba3d2d870d644ccd14d96dd8
KeaMedes <KeaMedes@gmail.com>
2014-11-03 17:29:52 +0800

    create leetcode1.cpp

---
+++ leetcode1.cpp
@@ -0,0 +1,14 @@
+/*
+* find minimum in rotated sorted array
+* by keamedes
+*/
+class DataIO{
+public:
+    void getData()
+};
+class Solution{
+public:
+    int findMin(std::vector<int> &num){
+
+    }
+}
